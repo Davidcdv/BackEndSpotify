9f87fd5217135f36d4ad59fa4191bcbfe5364bb8
Mac <mac@Macs-MacBook-Pro.local>
2024-12-05 12:26:09 +0100

    Add Socket.IO messaging feature to Spotify clone

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,11 +3,11 @@ import { Message } from "../models/message.model.js";
 
 export const initializeSocket = (server) => {
 	const io = new Server(server, {
-		cors: {
-			origin: "http://localhost:3000",
-			credentials: true,
-		},
-	});
+    cors: {
+      origin: "https://frontendspotify1.onrender.com",
+      credentials: true,
+    },
+  });
 
 	const userSockets = new Map(); // { userId: socketId}
 	const userActivities = new Map(); // {userId: activity}
